2a7cf698fc65a02dbada9cfeb31d87f8011a4fda
Ron Forbes <ronforbes528@gmail.com>
2018-09-21 13:23:05 -0700

    Updated Sign Up styling

--- src/components/screens/SignUp.js
+++ src/components/screens/SignUp.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, KeyboardAvoidingView, Text, Button, TextInput, StyleSheet } from 'react-native';
+import { View, KeyboardAvoidingView, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default class SignUp extends React.Component {
     state = {
@@ -7,6 +8,7 @@ export default class SignUp extends React.Component {
         email: '',
         password: '',
         passwordConfirmation: '',
+        guestName: '',
         error: ''
     }
 
@@ -25,8 +27,28 @@ export default class SignUp extends React.Component {
         event.preventDefault();
     }
 
+    handlePlayAsGuest = (event) => {
+        if(this.state.name !== '') {
+            firebase.auth().signInAnonymously().then(() => {
+                firebase.database().ref('.info/connected').on('value', snapshot => {
+                    if(snapshot.val()) {
+                        firebase.database().ref('players/' + firebase.auth().currentUser.uid).onDisconnect().remove();
+                        firebase.database().ref('players/' + firebase.auth().currentUser.uid).set({ name: this.state.name, playing: true });
+                    }
+                });
+                this.props.history.push('/home');
+            }).catch(error => {
+                this.setState({ playAsGuestError: error });
+            });
+        }
+    }
+
     onPressBack = () => {
         this.props.history.goBack();
+    }
+
+    onPressSignIn = () => {
+        this.props.history.push('signIn');
     }
 
     render() {
@@ -41,22 +63,26 @@ export default class SignUp extends React.Component {
             <View style={styles.container}>
                 <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundColor: "transparent" }}>
                     <View style={styles.header}>
-                        <Text style={styles.headerText}>Sign up to join!</Text>
+                        <FontAwesome.Button onPress={this.onPressBack} name="chevron-left" color="white" backgroundColor="transparent" style={styles.headerBackButton} />
+                        <Text style={styles.headerText}>SIGN UP</Text>
+                        <View style={styles.headerHorizontalBar} />
                     </View>
                     <View style={styles.form}>
-                        <Text style={styles.instructionsText}>Choose your Avatar!</Text>
-                        <View style={styles.avatarSelection}>
-                            <Button title="Male" onPress={() => {}} />
-                            <Button title="Female" onPress={() => {}} />
-                        </View>
+                        <Text style={styles.instructionsText}>Welcome to the Party!</Text>
+                        <Text style={styles.instructionsText}>Let's get started.</Text>
                         <TextInput value={this.state.name} onChangeText={text => { this.setState({ name: text }); }} placeholder="Player name" style={styles.formTextInput} />
                         <TextInput value={this.state.email} onChangeText={text => { this.setState({ email: text }); }} placeholder="Email address" keyboardType="email-address" textContentType="username" style={styles.formTextInput} />
                         <TextInput value={this.state.password} onChangeText={text => { this.setState({ password: text }); }} placeholder="Password" secureTextEntry textContentType="password" style={styles.formTextInput} />
                         <TextInput value={this.state.passwordConfirmation} onChangeText={text => { this.setState({ passwordConfirmation: text });  }} placeholder="Confirm password" secureTextEntry textContentType="password" style={styles.formTextInput} />
-                    </View>
-                    <View style={styles.footer}>
-                        <Button title="Back" onPress={this.onPressBack} style={styles.backButton} />
-                        <Button title="Start" onPress={this.handleSignUp} style={styles.startButton} disabled={isSignUpDisabled} />
+                        <Text style={styles.instructionsText}>{this.state.error && this.state.error.message}</Text>
+                        <Text style={styles.instructionsText}>Or...</Text>
+                        <TextInput value={this.state.guestName} onChangeText={text => { this.setState({ guestName: text }); }} placeholder="Player name" style={styles.formTextInput} />
+                        <Text onPress={this.handlePlayAsGuest} style={styles.playAsGuestLinkText}>Play as Guest</Text>
+                        <TouchableOpacity onPress={this.handleSignUp} disabled={isSignUpDisabled} style={styles.signUpButton}>
+                            <Text style={styles.signUpButtonText}>Sign up</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.signInLabel}>Have an account already?</Text>
+                        <Text onPress={this.onPressSignIn} style={styles.signInLink}>Sign in!</Text>
                     </View>
                 </KeyboardAvoidingView>
             </View>
@@ -71,23 +97,36 @@ const styles = StyleSheet.create({
     },
     header: {
         position: "absolute",
-        left: 0,
+        left: "10%",
         right: 0,
         top: 50,
-        height: 100,
-        backgroundColor: "white",
+        height: 50,
+        flexDirection: "row",
         justifyContent: "center",
         alignItems: "center"
     },
+    headerBackButton: {
+        justifyContent: "center" 
+    },
     headerText: {
-        fontSize: 24,
+        margin: 10,
+        fontSize: 14,
+        fontWeight: "bold",
+        color: "white"
+    },
+    headerHorizontalBar: {
+        flex: 1,
+        height: 12,
+        backgroundColor: "#FA50B2"
     },
     form: {
+        width: "80%",
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
     },
     instructionsText: {
+        width: "100%",
         fontSize: 24,
         color: "white"
     },
@@ -95,7 +134,7 @@ const styles = StyleSheet.create({
         flexDirection: "row"
     },
     formTextInput: {
-        minWidth: 200,
+        width: "100%",
         height: 40,
         fontSize: 24,
         textAlign: "center",
@@ -103,15 +142,31 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         margin: 10
     },
-    footer: {
-        position: "absolute",
-        left: 0,
-        right: 0,
-        bottom: 50,
-        height: 100,
-        backgroundColor: "white",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center"
-    }
+    playAsGuestLinkText: {
+        fontSize: 20,
+        color: "white",
+        fontWeight: "bold"
+    },
+    signUpButton: { 
+        width: "100%", 
+        margin: 10, 
+        padding: 10, 
+        backgroundColor: "#EE3AAF", 
+        borderWidth: 5, 
+        borderColor: "#B4287D" 
+    },
+    signUpButtonText: { 
+        fontSize: 24, 
+        textAlign: "center", 
+        color: "white" 
+    },
+    signInLabel: { 
+        fontSize: 20, 
+        color: "white" 
+    },
+    signInLink: { 
+        fontSize: 20, 
+        color: "#FA50B2", 
+        fontWeight: "bold" 
+    },
 });
