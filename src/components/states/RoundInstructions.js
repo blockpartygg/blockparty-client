fac08adeeb869e715bf10d6031ed58c7fa28b747
Ron Forbes <ronforbes528@gmail.com>
2018-09-28 12:37:41 -0700

    Updated Instructions screen to show which team player is on

--- src/components/states/RoundInstructions.js
+++ src/components/states/RoundInstructions.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Animated, Text } from 'react-native';
+import firebase from '../../Firebase';
 
 export default class RoundInstructions extends React.Component {
     state = {
         scaleAnimation: new Animated.Value(0)
     }
 
-    componentWillMount() {
+    componentDidMount() {
+        firebase.database.ref('game/teams/redTeamId').orderByValue().equalTo(firebase.uid).once('value', snapshot => {
+            if(snapshot.val()) {
+                this.setState({ team: "Red" });
+            }
+        });
+        firebase.database.ref('game/teams/blueTeamId').orderByValue().equalTo(firebase.uid).once('value', snapshot => {
+            if(snapshot.val()) {
+                this.setState({ team: "Blue" });
+            }
+        });
+
         Animated.timing(this.state.scaleAnimation, { toValue: 1, duration: 1000 }).start();
     }
 
     render() {
         return(
             <Animated.View style={{ position: "absolute", left: 0, right: 0, top: 0, bottom: 0, justifyContent: "center", alignItems: "center", transform: [{ scale: this.state.scaleAnimation }] }} pointerEvents="none">
-                <Text style={{ fontSize: 36, textAlign: "center", color: "white" }}>Round {this.props.round}</Text>
+                <Text style={{ fontSize: 36, textAlign: "center", color: "white" }}>{this.props.round && 'Round ' + this.props.round}</Text>
                 <Text style={{ fontSize: 48, textAlign: "center", color: "white" }}>{this.props.minigame && this.props.minigame.name}</Text>
                 <Text style={{ fontSize: 24, textAlign: "center", color: "white" }}>{this.props.minigame && this.props.minigame.instructions}{'\n'}</Text>
                 <Text style={{ fontSize: 48, textAlign: "center", color: "white" }}>{this.props.mode && this.props.mode.name}</Text>
                 <Text style={{ fontSize: 24, textAlign: "center", color: "white" }}>{this.props.mode && this.props.mode.instructions}</Text>
+                <Text style={{ fontSize: 24, textAlign: "center", color: "white" }}>{this.state.team && "You're on the "}<Text style={{ color: this.state.team === "Red" ? "red" : "blue" }}>{this.state.team}</Text> team</Text>
             </Animated.View>
         )
     }
