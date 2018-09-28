06010732405b291d95940c552a570fdb4a955dd4
Ron Forbes <ronforbes528@gmail.com>
2018-09-28 12:37:21 -0700

    Updated Countdown screen to show hours

--- src/components/states/PregameCountdown.js
+++ src/components/states/PregameCountdown.js
@@ -21,9 +21,10 @@ export default class PregameCountdown extends React.Component {
     update() {
         if(this.props.endTime != null) {
             let timeRemaining = new Date(new Date(this.props.endTime).getTime() - Date.now());
-            let minutes = timeRemaining.getMinutes();
+            let hours = timeRemaining.getUTCHours();
+            let minutes = timeRemaining.getMinutes() < 10 ? '0' + timeRemaining.getMinutes() : timeRemaining.getMinutes();
             let seconds = timeRemaining.getSeconds() < 10 ? '0' + timeRemaining.getSeconds() : timeRemaining.getSeconds();
-            let timeRemainingFormatted = `${minutes}:${seconds}`;
+            let timeRemainingFormatted = `${hours}:${minutes}:${seconds}`;
             this.setState({ timeRemaining: timeRemainingFormatted });
         }
     }
@@ -32,7 +33,7 @@ export default class PregameCountdown extends React.Component {
         return(
             <View style={{ position: "absolute", left: 0, right: 0, top: 0, bottom: 0, display: "flex", justifyContent: "center", alignItems: "center" }} pointerEvents="none">
                 <Text style={{ fontSize: 24, color: "white" }}>Game starts in...</Text>
-                <Text style={{ fontFamily: Platform.OS === 'ios' ? 'Helvetica Neue' : 'Roboto', fontSize: 182, color: "white" }}>{this.state.timeRemaining}</Text>
+                <Text style={{ fontFamily: Platform.OS === 'ios' ? 'Helvetica Neue' : 'Roboto', fontSize: 100, color: "white" }}>{this.state.timeRemaining}</Text>
             </View>
         )
     }
