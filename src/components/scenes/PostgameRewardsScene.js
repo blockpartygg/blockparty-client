b499164daaad56b03524a0b91801b091e0a15feb
Ron Forbes <ronforbes528@gmail.com>
2018-10-08 15:06:08 -0700

    Updated PostgameRewardsScene: Rewrote avatar rendering

--- src/components/scenes/PostgameRewardsScene.js
+++ src/components/scenes/PostgameRewardsScene.js
@@ -6,57 +6,21 @@ import '../../TeapotBufferGeometry';
 class PostgameRewardsScene {
     avatar = {
         currentScaleValue: 1,
-        nextScaleValue: 0.1
+        nextScaleValue: 0.000001
     }
 
     constructor(renderer) {
         this.renderer = renderer;
+    }
+
+    initialize() {
         this.setupScene();
         this.setupCamera();
         this.setupLights();
         this.setupBackground();
-
-        const geometries = [
-            new THREE.BoxGeometry(),
-            new THREE.SphereGeometry(0.5),
-            new THREE.ConeGeometry(0.5),
-            new THREE.CylinderBufferGeometry(0.5, 0.5),
-            new THREE.DodecahedronGeometry(0.5)
-        ];
-
-        let currentAvatarGeometry = geometries[0];
-        let avatarMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff, overdraw: 0.5 });
-        this.currentAvatarMesh = new THREE.Mesh(currentAvatarGeometry, avatarMaterial);
-        this.scene.add(this.currentAvatarMesh);
-
-        let nextAvatarGeometry = geometries[0];
-        this.nextAvatarMesh = new THREE.Mesh(nextAvatarGeometry, avatarMaterial);
-        this.scene.add(this.nextAvatarMesh);
-
-        this.initialize();
-
-        firebase.database.ref('players/' + firebase.uid + '/currentSkin').once('value', snapshot => {
-            let currentSkin = snapshot.val();
-            if(currentSkin !== null) {
-                currentAvatarGeometry = geometries[currentSkin];
-                this.scene.remove(this.currentAvatarMesh);
-                this.currentAvatarMesh = new THREE.Mesh(currentAvatarGeometry, avatarMaterial);
-                this.scene.add(this.currentAvatarMesh);
-            }
-        });
-
-        firebase.database.ref('players/' + firebase.uid + '/currentSkin').on('value', snapshot => {
-            let currentSkin = snapshot.val();
-            if(currentSkin !== null) {
-                nextAvatarGeometry = geometries[currentSkin];
-            }
-            else {
-                nextAvatarGeometry = geometries[0];
-            }
-            this.scene.remove(this.nextAvatarMesh);
-            this.nextAvatarMesh = new THREE.Mesh(nextAvatarGeometry, avatarMaterial);
-            this.scene.add(this.nextAvatarMesh);
-        });
+        this.setupAvatarGeometry();
+        this.setupFont();
+        this.setupPlayer();
     }
 
     setupScene() {
@@ -66,9 +30,9 @@ class PostgameRewardsScene {
 
     setupCamera() {
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        this.camera.position.x = 1;
-        this.camera.position.y = 1;
-        this.camera.position.z = 3;
+        this.camera.position.x = 0;
+        this.camera.position.y = 0;
+        this.camera.position.z = 5;
         this.camera.lookAt(new THREE.Vector3());
     }
 
@@ -92,59 +56,89 @@ class PostgameRewardsScene {
     setupBackground() {
         this.blocks = [];
         for(let i = 0; i < 100; i++) {
-            let size = Math.random() * 3;
-            let blockGeometry = new THREE.BoxGeometry(size, size, size);
-            let color = Math.random() * 0xffffff;
-            let blockMaterial = new THREE.MeshBasicMaterial({ color: color, wireframe: true });
+            const blockGeometry = new THREE.BoxBufferGeometry();
+            const color = Math.random() * 0xffffff;
+            const blockMaterial = new THREE.MeshBasicMaterial({ color: color, wireframe: true });
             this.blocks[i] = {};
             this.blocks[i].mesh = new THREE.Mesh(blockGeometry, blockMaterial);
             this.blocks[i].mesh.position.x = Math.random() * 50 - 25;
             this.blocks[i].mesh.position.y = Math.random() * 50 - 25;
-            this.blocks[i].mesh.position.z = -7;
+            this.blocks[i].mesh.position.z = -Math.random() * 25;
+            const scale = Math.random();
+            this.blocks[i].mesh.scale.x = scale;
+            this.blocks[i].mesh.scale.y = scale;
+            this.blocks[i].mesh.scale.z = scale;
             this.blocks[i].mesh.rotation.x = Math.random() * 2 * Math.PI;
             this.blocks[i].mesh.rotation.y = Math.random() * 2 * Math.PI;
+            this.blocks[i].mesh.rotation.z = Math.random() * 2 * Math.PI;
             this.scene.add(this.blocks[i].mesh);
         }
     }
 
-    initialize() {
-        this.avatar.scaleValue = 1;
-        this.avatar.scaleValue2 = 0;
-        this.currentAvatarMesh.scale.x = 1;
-        this.currentAvatarMesh.scale.y = 1;
-        this.currentAvatarMesh.scale.z = 1;
-        this.nextAvatarMesh.scale.x = 0.1;
-        this.nextAvatarMesh.scale.y = 0.1;
-        this.nextAvatarMesh.scale.z = 0.1;
+    setupAvatarGeometry() {
+        this.avatarGeometry = [
+            new THREE.BoxGeometry(),
+            new THREE.SphereGeometry(0.5),
+            new THREE.ConeGeometry(0.5),
+            new THREE.CylinderBufferGeometry(0.5, 0.5),
+            new THREE.DodecahedronGeometry(0.5)
+        ];
     }
 
-    onTouchesBegan(state) {
-        
+    setupFont() {
+        const fontLoader = new THREE.FontLoader();
+        this.font = fontLoader.parse(require('../../assets/fonts/helvetiker_regular.typeface.json'));
     }
+
+    setupPlayer() {
+        this.playerGroup = new THREE.Group();
+        this.playerGroup.rotation.x = Math.PI / 6;
+        this.playerGroup.rotation.y = Math.PI / 6;
+        this.scene.add(this.playerGroup);
+
+        firebase.database.ref('players/' + firebase.uid).on('value', snapshot => {
+            let player = snapshot.val();
+
+            // setup player avatar mesh
+            if(this.playerAvatarMesh) {
+                this.playerGroup.remove(this.playerAvatarMesh);
+            }
+            const avatarGeometry = this.avatarGeometry[player.currentSkin || 0];
+            const avatarMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff, overdraw: 0.5 });
+            this.playerAvatarMesh = new THREE.Mesh(avatarGeometry, avatarMaterial);
+            this.playerGroup.add(this.playerAvatarMesh);
+
+            // setup player name mesh
+            if(this.playerNameMesh) {
+                this.playerGroup.remove(this.playerNameMesh);
+            }
+            const nameGeometry = new THREE.TextGeometry(player.name, { font: this.font, size: 0.5, height: 0 });
+            const nameMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+            this.playerNameMesh = new THREE.Mesh(nameGeometry, nameMaterial);
+            nameGeometry.computeBoundingBox();
+            this.playerNameMesh.position.x = -nameGeometry.boundingBox.max.x / 2;
+            this.playerNameMesh.position.y = 0.5;
+            this.playerGroup.add(this.playerNameMesh);
+        });
+    }
+
+    onTouchesBegan(state) {}
 
     startPurchase() {
-        this.avatar.currentScaleValue = 1;
-        this.avatar.nextScaleValue = 0;
-        this.currentAvatarMesh.scale.x = 1;
-        this.currentAvatarMesh.scale.y = 1;
-        this.currentAvatarMesh.scale.z = 1;
-        this.nextAvatarMesh.scale.x = 0.1;
-        this.nextAvatarMesh.scale.y = 0.1;
-        this.nextAvatarMesh.scale.z = 0.1;
-        TweenLite.to(this.avatar, 2, { currentScaleValue: 0.1 });
-        TweenLite.to(this.avatar, 2, { nextScaleValue: 1, delay: 3 });
+        TweenLite.to(this.playerGroup.rotation, 1, { y: 4 * Math.PI, onComplete: () => { 
+            this.playerGroup.rotation.x = Math.PI / 6;
+            this.playerGroup.rotation.y = Math.PI / 6;
+        }});
+        TweenLite.to(this.playerGroup.scale, 1, { x: 0, y: 0, z: 0 });
+        TweenLite.to(this.playerGroup.scale, 3, { x: 1, y: 1, z: 1, delay: 2 });
+        TweenLite.to(this.playerGroup.rotation, 3, { y: 4 * Math.PI + Math.PI / 6, onCompelte: () => {
+            this.playerGroup.rotation.x = Math.PI / 6;
+            this.playerGroup.rotation.y = Math.PI / 6;
+        }});
     }
 
     update() {
         this.updateBackground();
-
-        this.currentAvatarMesh.scale.x = this.avatar.currentScaleValue;
-        this.currentAvatarMesh.scale.y = this.avatar.currentScaleValue;
-        this.currentAvatarMesh.scale.z = this.avatar.currentScaleValue;
-
-        this.nextAvatarMesh.scale.x = this.avatar.nextScaleValue;
-        this.nextAvatarMesh.scale.y = this.avatar.nextScaleValue;
-        this.nextAvatarMesh.scale.z = this.avatar.nextScaleValue;
     }
 
     updateBackground() {
@@ -168,7 +162,9 @@ class PostgameRewardsScene {
         }
     }
 
-    shutdown() {}
+    shutdown() {
+        firebase.database.ref('players/' + firebase.uid).off();
+    }
 }
 
 export default PostgameRewardsScene;
