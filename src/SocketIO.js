3a112549c0bec6ab619c1d2385e012942f3ffa31
Ron Forbes <ronforbes528@gmail.com>
2018-10-11 10:45:38 -0700

    Updated SocketIO: updated dev server URI

--- src/SocketIO.js
+++ src/SocketIO.js
@@ -5,8 +5,7 @@ class SocketIO {
 
     initialize() {
         // On Heroku, http server port is set dynamically but always listens for connections on default port 80
-        let uri = process.env.NODE_ENV === 'production' ? 'http://blockparty-server.herokuapp.com' : 'http://172.20.10.2:1337';
-        console.log('attempting to connect to ' + uri);
+        let uri = process.env.NODE_ENV === 'production' ? 'http://blockparty-server.herokuapp.com' : 'http://192.168.86.48:1337';
         this.socket = socketIO(uri);
         this.socket.on('connect_error', error => {
             console.log("SocketIO: Event: 'connect_error': error=");
